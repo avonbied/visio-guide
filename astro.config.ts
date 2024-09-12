5ffbbf76c648233b8749437f74111c59619c9626
Lex <58815784+avonbied@users.noreply.github.com>
2024-09-12 00:13:55 -0500

    Fix astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://avonbied.github.io',
-	base: '/visio-guide'
+	base: '/visio-guide',
 	integrations: [sitemap()],
 	server: {
 		port: Number(process.env.PORT ?? 8080),
