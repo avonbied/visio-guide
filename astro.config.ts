b5bb368b1fa3a4f6a09eb8031975d1d5f34ec46e
Lex <58815784+avonbied@users.noreply.github.com>
2024-09-12 00:12:45 -0500

    Initialize repository from template

---
+++ astro.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://avonbied.github.io',
+	base: '/visio-guide'
+	integrations: [sitemap()],
+	server: {
+		port: Number(process.env.PORT ?? 8080),
+		host: true,
+	},
+	vite: {
+		server: {
+			watch: {
+				usePolling: true,
+				interval: 10000
+			}
+		}
+	}
+});
